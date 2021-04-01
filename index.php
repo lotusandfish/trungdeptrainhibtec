a7ff73c25ababde3878ec239666340107d26184d
lotusandfish <70317796+lotusandfish@users.noreply.github.com>
2021-04-01 16:14:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <?php 
-  session_start();
-  if isset($_POST['login']){
-    $uname = $_POST['uname'];
-    $pwd = $_POST['pwd'];
-
-    $sql = "SELECT * FROM tbl_user WHERE username = '$uname' and password = '$pwd'"; 
-    $query = pg_query($conn,$sql);
-    $login_check = pg_num_rows($query);
-    if($login_check == 1){ 
-        $_SESSION['user'] = $uname;
-        header('Location: welcome.php');    
-    }else{
-        echo "Invalid Details";
-    }
-
-  }
+	session_start();
+	$conn_string = "host=ec2-23-21-229-200.compute-1.amazonaws.com port=5432 dbname=d45cnbrd3f2d7l user=qqhhlmrdigauql password=637e31a7084d8a47a567ba0f2fb9d51707920f4751f53b734bb438a6485f1395";
+	$conn = pg_connect($conn_string);
+	if(isset($_POST['login'])){
+	    $uname = $_POST['uname'];
+	    $pwd = $_POST['psw'];
+	    $sql = "SELECT * FROM tbl_user WHERE username = '$uname' and password = '$pwd'"; 
+	    $query = pg_query($conn,$sql);
+	    $login_check = pg_num_rows($query);
+	    if($login_check == 1){ 
+		$_SESSION['user'] = $uname;
+        	header("Location: welcome.php");
+	    }else{
+		echo "Invalid Details";
+	    }
+	
+	 }
 ?>
 <!DOCTYPE html>
 <html>
